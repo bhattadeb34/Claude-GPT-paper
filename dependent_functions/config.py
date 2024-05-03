6db4def281d13d00709568417d8ed0f1c9d5cf5d
bhattadeb34 <106618568+bhattadeb34@users.noreply.github.com>
2024-05-03 00:24:49 -0400

    Create config.py

---
+++ dependent_functions/config.py
@@ -0,0 +1,8 @@
+# config.py
+import os
+
+
+# Set the data_home path to your Google Drive directory
+data_home = '/content/drive/MyDrive/FACT_EFRC/Claude-GPT-paper/dependent_functions'
+
+fact_dropbox_path='/content/drive/MyDrive/FACT_EFRC/Claude-GPT-paper/fact-dropbox'
